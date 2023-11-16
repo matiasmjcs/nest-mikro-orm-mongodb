b8d536de6b4631f7f1e65ddbaa330aa971f15969
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-11-16 17:23:58 -0300

    #	modified:   src/database/database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,18 +1,20 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ApplicationEnvironment } from 'src/application-environment/entities/application-environment.entity';
 import { Application } from 'src/application/entities/application.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     MikroOrmModule.forRoot({
       entities: [Application, ApplicationEnvironment],
       entitiesTs: [Application, ApplicationEnvironment],
       type: 'mongo',
       port: 27017,
-      user: 'root',
-      password: 'example',
-      dbName: 'nest-app',
+      user: process.env.USER_NAME,
+      password: process.env.PASSWORD,
+      dbName: process.env.DB_NAME,
       debug: true,
       persistOnCreate: true,
     }),
